b2f1136eca1944b5cd9a76a767ebf419089876df
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-08-07 20:07:03 +0500

    added microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :micropsts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
   root 'users#index'
-  resources :users
+
   
 end
