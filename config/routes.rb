04c8555febbae4edd0911466d4fe20f64a81aa59
imhta <imhtapm@gmail.com>
2019-08-08 14:32:46 +0530

    .gitignore updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :micropsts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'micropsts#index'
   root 'users#index'
 
   
