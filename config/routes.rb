c9f4b3c30a03c37dd55d18a5dc1c5613bebc6f22
imhta <imhtapm@gmail.com>
2019-08-08 20:25:51 +0530

    without root route for root micropst

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :micropsts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'micropsts#index'
   root 'users#index'
 
   
